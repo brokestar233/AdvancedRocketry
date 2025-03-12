4c3e67e43062a37c5a244b4505ec5a0413d2d573
brokestar233 <161171586+brokestar233@users.noreply.github.com>
2025-03-13 01:51:00 +0800

    Update RendererObservatory.java

--- src/main/java/zmaster587/advancedRocketry/client/render/multiblocks/RendererObservatory.java
+++ src/main/java/zmaster587/advancedRocketry/client/render/multiblocks/RendererObservatory.java
@@ -14,27 +14,117 @@ import net.minecraftforge.client.model.AdvancedModelLoader;
 import net.minecraftforge.client.model.IModelCustom;
 import net.minecraftforge.common.util.ForgeDirection;
 
-public class RendererObservatory  extends TileEntitySpecialRenderer {
+public class RendererObservatory extends TileEntitySpecialRenderer {
 
-	final IModelCustom model = AdvancedModelLoader.loadModel(new ResourceLocation("advancedrocketry:models/observatory.obj"));
+    private final IModelCustom model = AdvancedModelLoader.loadModel(new ResourceLocation("advancedrocketry:models/observatory.obj"));
 
-	@NotNull
-    final
-    ResourceLocation texture = new ResourceLocation("advancedrocketry:textures/models/T1Observatory.png");
+    @NotNull
+    final ResourceLocation texture = new ResourceLocation("advancedrocketry:textures/models/T1Observatory.png");
 
-	private static int bodyList;
+    // 改为非静态变量并通过volatile保证线程可见性
+    private volatile int bodyList = -1; 
+    private boolean isDisplayListGenerated = false;
 
-	public RendererObservatory() {
-		GL11.glNewList(bodyList = GL11.glGenLists(1), GL11.GL_COMPILE);
-		model.renderOnly("body");
-		GL11.glEndList();
-	}
+    @Override
+    public void renderTileEntityAt(TileEntity tile, double x, double y, double z, float f) {
+        TileObservatory multiBlockTile = (TileObservatory) tile;
 
+        if (!multiBlockTile.canRender())
+            return;
 
-	@Override
-	public void renderTileEntityAt(TileEntity tile, double x,
-			double y, double z, float f) {
-		TileObservatory multiBlockTile = (TileObservatory)tile;
+        // 延迟初始化显示列表
+        if (!isDisplayListGenerated) {
+            generateDisplayList();
+            isDisplayListGenerated = true;
+        }
+
+        GL11.glPushMatrix();
+
+        // 光照计算
+        int bright = tile.getWorldObj().getLightBrightnessForSkyBlocks(tile.xCoord, tile.yCoord + 2, tile.zCoord, 0);
+        OpenGlHelper.setLightmapTextureCoords(OpenGlHelper.lightmapTexUnit, bright % 65536, bright / 65536);
+
+        // 坐标变换
+        ForgeDirection front = RotatableBlock.getFront(tile.getBlockMetadata());
+        GL11.glTranslated(x + .5, y, z + .5);
+        GL11.glRotatef((front.offsetX == 1 ? 180 : 0) + front.offsetZ * 90f, 0, 1, 0);
+        GL11.glTranslated(2, -1, 0);
+
+        bindTexture(texture);
+
+        float offset = multiBlockTile.getOpenProgress();
+
+        // 动态渲染逻辑
+        if (offset != 0f) {
+            renderDynamicParts(offset);
+        } else {
+            renderStaticParts();
+        }
+
+        GL11.glPopMatrix();
+    }
+
+    private void generateDisplayList() {
+        // 保障线程安全
+        if (GL11.glGetCapabilities() == null) return;
+
+        // 生成新的显示列表
+        bodyList = GL11.glGenLists(1);
+        
+        // 手动平衡OpenGL状态
+        GL11.glPushAttrib(GL11.GL_ALL_ATTRIB_BITS);
+        GL11.glPushMatrix();
+
+        GL11.glNewList(bodyList, GL11.GL_COMPILE);
+        model.renderOnly("body");
+        GL11.glEndList();
+
+        GL11.glPopMatrix();
+        GL11.glPopAttrib();
+    }
+
+    private void renderDynamicParts(float offset) {
+        if (Debugger.renderList && bodyList != -1) {
+            GL11.glCallList(bodyList);
+        } else {
+            model.renderOnly("Base");
+        }
+
+        model.renderPart("Scope");
+        model.renderPart("Axis");
+
+        // 动态部件渲染
+        GL11.glPushMatrix();
+        GL11.glTranslatef(0, 0, -offset);
+        model.renderOnly("CasingXMinus");
+        GL11.glPopMatrix();
+
+        GL11.glPushMatrix();
+        GL11.glTranslatef(0, 0, offset);
+        model.renderOnly("CasingXPlus");
+        GL11.glPopMatrix();
+    }
+
+    private void renderStaticParts() {
+        if (Debugger.renderList && bodyList != -1) {
+            GL11.glCallList(bodyList);
+        } else {
+            model.renderOnly("Base");
+        }
+        model.renderOnly("CasingXMinus");
+        model.renderOnly("CasingXPlus");
+    }
+
+    @Override
+    public void onRenderWorldUnload() {
+        // 安全释放显示列表
+        if (bodyList != -1) {
+            GL11.glDeleteLists(bodyList, 1);
+            bodyList = -1;
+            isDisplayListGenerated = false;
+        }
+    }
+}		TileObservatory multiBlockTile = (TileObservatory)tile;
 
 		if(!multiBlockTile.canRender())
 			return;
